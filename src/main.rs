5b0432f91dad750ca79f430cd8d3d9ce6e105ede
Mark Smith <smith.h.mark@gmail.com>
2022-03-09 15:44:08 -0500

    adding some tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+
+fn message() -> String {
+    String::from("Hello world")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let msg = message();
+    println!("{}", msg);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn message_startswith_hello() {
+        let expected = String::from("Hello");
+        assert_eq!(expected, message()[..expected.len()]);
+        assert!(message().starts_with(&expected));
+    }
+
+    #[test]
+    fn message_endswith_world() {
+        let expected = String::from("world");
+        let msg = message();
+        assert_eq!(expected, msg[msg.len()-expected.len()..]);
+        assert!(msg.ends_with(&expected));
+    }
 }
